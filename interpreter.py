63d0fe0b37ae96b3375afff308085803dc47cdcd
joshop <39889154+joshop@users.noreply.github.com>
2021-03-20 19:15:37 -0400

    Postfix counter change
    Postfix counter ("return") should add two to the address before going there to jump over the call itself.

--- interpreter.py
+++ interpreter.py
@@ -156,7 +156,7 @@ class CButNot:
         if position == 'prefix':
             return self.command_idx
         else:
-            self.command_idx = value
+            self.command_idx = value + 2
             raise JumpException
     def cmd_count(self, position, value):
         return int(position == 'prefix') - value

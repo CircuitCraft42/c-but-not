475d0cf0395911957d305597882530e08bfd711c
joshop <joshop4646@gmail.com>
2021-03-20 11:57:21 -0400

    Added a samples directory with a hello world sample

---
+++ samples/hello-world.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+	int i;
+	int buffer;
+	i += 72;
+	i += buffer;
+	i += 101;
+	i += buffer;
+	i += 108;
+	i += buffer;
+	i += 108;
+	i += buffer;
+	i += 111;
+	i += buffer;
+	i += 32;
+	i += buffer;
+	i += 119;
+	i += buffer;
+	i += 111;
+	i += buffer;
+	i += 114;
+	i += buffer;
+	i += 108;
+	i += buffer;
+	i += 100;
+	i += buffer;
+	i += 10;
+	i += buffer;
+}

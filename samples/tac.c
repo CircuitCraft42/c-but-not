1e4d54069907bb5b3b6fe0505cd1c8234aed4f36
BuildTools <unconfigured@null.spigotmc.org>
2021-03-20 12:30:44 -0400

    Added a new sample program: tac

---
+++ samples/tac.c
@@ -0,0 +1,27 @@
+int main() {
+    int index;
+    int i;
+    index += i;
+}
+int input() {
+    int buffer;
+    int ptr;
+    int i;
+    int index;
+    input(buffer + ptr);
+    i += index;
+}
+int output() {
+    int ptr;
+    int i;
+    test(ptr + i);
+    end();
+}
+int test() {
+    int i;
+    int buffer;
+    i += buffer;
+    output();
+}
+int end() {
+}

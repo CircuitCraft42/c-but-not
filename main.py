246db960cf19579667cf9b53d6dcc4c336d36b88
joshop <joshop4646@gmail.com>
2021-03-20 17:28:07 -0400

    Programs now must be valid C to be interpreted successfully

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-import sys
+import sys, os
 import parsing
 import interpreter
 if len(sys.argv) != 2:
     print("Usage: " + sys.argv[0] + " <filename>")
 else:
-    code = open(sys.argv[1]).read()
-    instructions = parsing.parse(code)
-    runner = interpreter.CButNot(instructions)
-    runner.run()
+    retcode = os.system("cc " + sys.argv[1] + " -o /dev/null > /dev/null 2> /dev/null")
+    if retcode:
+        print("ERROR: Program is not valid C.")
+    else:
+        code = open(sys.argv[1]).read()
+        instructions = parsing.parse(code)
+        runner = interpreter.CButNot(instructions)
+        runner.run()

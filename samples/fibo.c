a6e8f9fe1b63d0d80b51f79a3ca411203f9af252
joshop <joshop4646@gmail.com>
2021-03-20 19:24:54 -0400

    Added recursive fibonacci test program

---
+++ samples/fibo.c
@@ -0,0 +1,50 @@
+int i;
+int table;
+int ptr;
+int counter;
+int buffer;
+int cnt;
+int cnt_ptr;
+int ptr_idx;
+int index_ptr;
+int main() {
+	i += 10;
+	i += ptr;
+	counter += ptr;
+	fibo();
+	ptr += buffer; // prints 7 == ascii 55
+	i += 10;
+	i += buffer;
+	end();
+}
+int fibo() {
+	// fibo: fibonacci sequence. Takes one parameter (which term to compute) and returns that term.
+	i &= 2;
+	table &= i; // swap
+	ptr += ptr; // and pop into +
+	i |= 2;
+	ptr *= i;
+	i |= ptr;
+	fibo_recurse(cnt * i); // goto if argument >= 3
+	i += 1; // return 1 in this case
+	i += ptr; // drop and push...
+	table &= i; // ...swap...
+	ptr += counter; // ...and ret
+}
+int fibo_recurse(int a) {
+	i += cnt_ptr;
+	i += cnt_ptr;
+	counter += ptr;
+	fibo(); // recurse!
+	i &= 2;
+	table &= i; // swap
+	counter += ptr;
+	fibo(); // and recurse again!
+	ptr_idx += ptr;
+	i &= 2;
+	table &= i; // swap for the last time
+	ptr += counter; // ...and ret
+}
+int end() {
+
+}

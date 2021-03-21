19df25a03c85703d6fded4154136059969ec5799
BuildTools <unconfigured@null.spigotmc.org>
2021-03-21 00:09:52 -0400

    Shortened the tac example and added a regshift test.

---
+++ samples/regshift-test.c
@@ -0,0 +1,11 @@
+int i;
+int y_y_idx;
+int index;
+int buffer;
+
+int main() {
+	i += 30;
+	i -= 59;
+	y_y_idx *= index;
+	i *= buffer;
+}
--- samples/tac.c
+++ samples/tac.c
@@ -1,25 +1,20 @@
+int index;
+int i;
+int buffer;
+int ptr;
+
 int main() {
-    int index;
-    int i;
     index += i;
 }
 int input() {
-    int buffer;
-    int ptr;
-    int i;
-    int index;
     input(buffer + ptr);
     i += index;
 }
 int output() {
-    int ptr;
-    int i;
     test(ptr + i);
     end();
 }
 int test() {
-    int i;
-    int buffer;
     i += buffer;
     output();
 }
